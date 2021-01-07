1dd9ceedb610604b4d2d9c4f9ec381c7181edc58
ocoleman <owencoleman@outlook.ie>
2021-01-07 23:57:17 +0000

    Added polynomial model

--- models.py
+++ models.py
@@ -3,24 +3,49 @@ import tensorflow.keras as kr
 import pandas as pd
 
 df = pd.read_csv("powerproduction.csv")
+#Remove the unreasonable power values
+zeroVals = df[(df["speed"] > 10) & (df["power"] == 0)].index
+df.drop(zeroVals, inplace=True)
 x = np.array(df["speed"])
 y = np.array(df["power"])
 
 class linear_model:
 
-    def train(self):
-        self.model.add(kr.layers.Dense(1, input_shape=(1,), activation="linear", kernel_initializer='ones', bias_initializer='zeros'))
-        self.model.compile('adam', loss='mean_squared_error')
-        # Train the neural network on our training data.
-        self.model.fit(x, y, epochs=500)
-
     def __init__(self, speed, power):
         self.model = kr.models.Sequential()
+        self.model.add(kr.layers.Dense(1, input_shape=(1,), activation="linear", kernel_initializer='ones', bias_initializer='zeros'))
+        self.model.compile('adam', loss='mean_squared_error')
         self.x = speed
         self.y = power
     
+    def train(self):
+        # Train the neural network on our dataset.
+        self.model.fit(x, y, epochs=1500)
+
     def predict(self, val):
         prediction = self.model.predict([val])
         return prediction.item(0)
 
+class polynomial_model:
+
+    def __init__(self, speed, power):
+        self.model = kr.models.Sequential()
+        self.model.add(kr.layers.Dense(30, input_shape=(1,), activation='sigmoid'))
+        self.model.add(kr.layers.Dense(1, activation='linear'))
+        self.model.compile(kr.optimizers.Adam(lr=0.001), loss='mean_squared_error')
+        self.x = speed
+        self.y = power
+    
+    def train(self):
+        # Train the neural network on our dataset.
+        self.model.fit(x, y, epochs=300, batch_size=5)
+
+    def predict(self, val):
+        prediction = self.model.predict([val])
+        return prediction.item(0)
+
+
+
+
 linmodel = linear_model(x, y)
+polymodel = polynomial_model(x, y)
--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 from flask import Flask, url_for, request, abort, redirect, jsonify, session, render_template, url_for
-from models import linmodel
+from models import linmodel, polymodel
 app = Flask(__name__, template_folder='static')
+
+lincalled = False
+polycalled = False
 
 @app.route('/')
 def home():
@@ -16,10 +19,36 @@ def linear():
 
     return jsonify({'error' : 'Missing input value!'})
 
-@app.route('/linear_init')
+@app.route('/polynomial', methods=['POST'])
+def polynomial():
+    value = request.form['polyValue']
+
+    if value:
+        return jsonify({'polyValue' : polymodel.predict(float(value))})
+
+    return jsonify({'error' : 'Missing input value!'})
+
+
+@app.route('/linear_init', methods=['GET'])
 def linear_init():
-    linmodel.train()
-    return render_template('index.html')
+    global lincalled
+    if lincalled == False:
+        linmodel.train()
+    
+    lincalled = True
+
+    return jsonify({'value' : 'STATUS: Linear Model Loaded.'})
+
+@app.route('/poly_init', methods=['GET'])
+def poly_init():
+    global polycalled
+    if polycalled == False:
+        polymodel.train()
+        
+    polycalled = True
+
+    return jsonify({'value' : 'STATUS: Polynomial Model Loaded.'})
+
 
 if __name__ == "__main__":
-    app.run(debug=True) #allows for realtime editing
+    app.run() 
